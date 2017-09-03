5e6fb07c2917063c0f66647036a5a73103b3a837
Ishita Jain <ishita.jain@scotiabank.com>
2017-09-03 18:43:56 -0400

    added hello world example

---
+++ app/components/basic/HelloWorld.js
@@ -0,0 +1,20 @@
+/**
+ * Created by ishitajain on 2017-09-03.
+ */
+import React from 'react';
+
+
+export default class HelloWorld extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return (<div>
+            Hello World
+        </div>);
+    }
+}
