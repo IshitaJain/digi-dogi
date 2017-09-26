d16b04b59dd577f9cb0e134fba48afa0d57bcc21
Ishita Jain <ishita.jain@scotiabank.com>
2017-09-26 19:23:40 -0400

    remove fail on error

--- app/components/basic/EmployeeView.js
+++ app/components/basic/EmployeeView.js
@@ -6,22 +6,36 @@ export class EmployeeView extends React.Component {
 
     constructor(props) {
         super(props);
-        console.log(props);
         this.state = {
-            employeeId: this.props.match.params.employeeId
+            employeeId: this.props.match.params.employeeId,
+            employee: null
         };
+    }
+
+    componentWillMount() {
+        let employee = null;
+        this.props.employeesList.map((emp) => {
+            if (emp.id == this.state.employeeId) {
+                employee = emp;
+            }
+        });
+
+        this.setState({
+            employee: employee
+        });
     }
 
     render() {
         return (<div>
-            <h1>Hello</h1>
+            <h1>Hello {this.state.employee.name}</h1>
 
         </div>);
     }
 }
 
 EmployeeView.propTypes = {
-    match: PropTypes.any
+    match: PropTypes.any,
+    employeesList: PropTypes.array
 };
 
 const mapStateToProps = (state) => {
--- webpack.config.js
+++ webpack.config.js
@@ -26,16 +26,11 @@ module.exports = {
         }),
         new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin(),
         new webpack.DefinePlugin({
           'process.env.NODE_ENV': JSON.stringify('development')
         })
     ],
-    eslint: {
-        configFile: '.eslintrc',
-        failOnWarning: false,
-        failOnError: false
-    },
+    eslint: {},
     module: {
         preLoaders: [
             {
